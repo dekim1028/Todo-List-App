d21e6912e884893ff51106323aeeb6658de4019c
dekim1028 <daeun235194@gmail.com>
2021-01-10 11:22:14 +0900

    Add card view

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View, TextInput} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -7,11 +7,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#3143e8',
   },
   appTitle: {
-    color: 'white',
-    textAlign: 'center',
+    color: '#fff',
     fontSize: 36,
     marginTop: 30,
     marginBottom: 30,
+    fontWeight: '300',
+    textAlign: 'center',
+    backgroundColor: '#3143e8',
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 30,
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24,
+    marginLeft: 20,
   },
 });
 
@@ -19,6 +37,9 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.appTitle}>Hello World</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder="Add an item!" />
+      </View>
     </SafeAreaView>
   );
 };
